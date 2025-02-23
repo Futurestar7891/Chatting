e6debfea5afb247fe1ce3b2371ab4290ae3ea526
Futurestar7891 <gajanandmali69@gmail.com>
2025-02-23 10:04:08 +0530

    add roues

--- frontend/chatapp/src/Components/Login.jsx
+++ frontend/chatapp/src/Components/Login.jsx
@@ -71,7 +71,7 @@ const Login = () => {
         requestBody.Mobile = formData.email;
       }
 
-      const response = await fetch("http://localhost:3000/api/login", {
+      const response = await fetch("https://chat-chit-six.vercel.app/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
